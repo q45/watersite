f26f862c914ec1f2eeb9e9fc63d938d6165b5062
q45 <quintinsmith@gmail.com>
2013-02-15 14:19:36 -0700

    First Commit for Utah Water Systems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaterSite::Application.config.secret_token = 'a9dda5937d7199940b9665de0864f5e0e43349cbfb296808167a7defcec0d792c34644cb5f4729fad4f97fdc1440f597d7097c628f734630a9ce359139af81d0'
